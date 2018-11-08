609fafe0680329724c543b6bd5eefb20ac34058c
codehandbook <codehandbook@gmail.com>
2018-11-08 20:55:09 +0530

    adding project

---
+++ routes/users.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const db = require('../models/index');
+const User = db.sequelize.models.User;
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+	return User.findAll({
+		raw: true
+	})
+	.then(function(result){
+		res.send(result);
+	})
+});
+
+module.exports = router;
